25d4d6cf03e58c07fbc137dff4173426d78119e8
pmrelvas <pedromiguelrelvas@gmail.com>
2016-06-27 22:47:45 +0100

    adicao de ficheiros base do porjeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FlappyPR
+
+Feito com base nos tutoriais do Brent Aureli https://www.youtube.com/watch?v=rzBVTPaUUDg&list=PLZm85UZQLd2TPXpUJfDEdWTSgszionbJy
---
+++ desktop/src/com/pelezinorr/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pelezinorr.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pelezinorr.game.FlappyPR;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyPR(), config);
+	}
+}
