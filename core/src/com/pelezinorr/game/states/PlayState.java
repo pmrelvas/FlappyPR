c673e09822659e56f432e495eaacbf9732d14532
pmrelvas <pedromiguelrelvas@gmail.com>
2016-06-27 23:56:50 +0100

    tut 8 - adicao de um tubo

--- core/src/com/pelezinorr/game/states/PlayState.java
+++ core/src/com/pelezinorr/game/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pelezinorr.game.FlappyPR;
 import com.pelezinorr.game.sprites.Bird;
+import com.pelezinorr.game.sprites.Tube;
 
 /**
  * Created by pedro on 27-06-2016.
@@ -12,11 +13,13 @@ import com.pelezinorr.game.sprites.Bird;
 public class PlayState extends State {
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
         cam.setToOrtho(false, FlappyPR.WIDTH/2, FlappyPR.HEIGHT/2);
     }
 
@@ -39,6 +42,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth/2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
