f505c0d5241c0a48c90a72323e610f2ff0097130
pmrelvas <pedromiguelrelvas@gmail.com>
2016-06-27 23:46:24 +0100

    tut 7 - adicao de fundo e limitacao de chao

--- core/src/com/pelezinorr/game/states/PlayState.java
+++ core/src/com/pelezinorr/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package com.pelezinorr.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pelezinorr.game.FlappyPR;
@@ -10,16 +11,20 @@ import com.pelezinorr.game.sprites.Bird;
  */
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
+        bg = new Texture("bg.png");
         cam.setToOrtho(false, FlappyPR.WIDTH/2, FlappyPR.HEIGHT/2);
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
@@ -32,6 +37,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth/2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
