4b1065ce5688dea38f1fd848eb65ad7c2fd12cd6
pmrelvas <pedromiguelrelvas@gmail.com>
2016-06-27 23:20:47 +0100

    tutorial quatro - adicao de fundo no menu principal

--- core/src/com/pelezinorr/game/states/MenuState.java
+++ core/src/com/pelezinorr/game/states/MenuState.java
@@ -1,13 +1,20 @@
 package com.pelezinorr.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.pelezinorr.game.FlappyPR;
 
 /**
  * Created by pedro on 27-06-2016.
  */
 public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
     }
 
     @Override
@@ -22,6 +29,9 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyPR.WIDTH, FlappyPR.HEIGHT);
+        sb.draw(playBtn, (FlappyPR.WIDTH/2)-(playBtn.getWidth()/2), FlappyPR.HEIGHT/2);
+        sb.end();
     }
 }
