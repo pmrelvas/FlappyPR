0d3df6e2c4835b0074a27d8f90d5142a85a3292c
Pedro Relvas <pedromiguelrelvas@gmail.com>
2016-09-16 00:30:38 +0100

    adicao de labels (score e titulo) e de sons

--- core/src/com/pelezinorr/game/states/MenuState.java
+++ core/src/com/pelezinorr/game/states/MenuState.java
@@ -1,8 +1,13 @@
 package com.pelezinorr.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.pelezinorr.game.FlappyPR;
 
 /**
@@ -11,12 +16,16 @@ import com.pelezinorr.game.FlappyPR;
 public class MenuState extends State {
     private Texture background;
     private Texture playBtn;
+    private Stage stage;
+    private Label lblTitle, lblDeveloper, lblVersion;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        stage = new Stage();
         cam.setToOrtho(false, FlappyPR.WIDTH/2, FlappyPR.HEIGHT/2);
         background = new Texture("bg.png");
         playBtn = new Texture("playbtn.png");
+        initLabels();
     }
 
     @Override
@@ -29,6 +38,7 @@ public class MenuState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        stage.act(dt);
     }
 
     @Override
@@ -38,11 +48,36 @@ public class MenuState extends State {
         sb.draw(background, 0, 0);
         sb.draw(playBtn, cam.position.x - playBtn.getWidth()/2, cam.position.y);
         sb.end();
+        stage.draw();
     }
 
     @Override
     public void dispose() {
         background.dispose();
         playBtn.dispose();
+    }
+
+    private void initLabels() {
+        lblTitle = new Label("Flappy Birds \n \"No Buraco\"", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        lblTitle.setSize(50,20);
+        lblTitle.setPosition(FlappyPR.WIDTH-FlappyPR.WIDTH/2-25, FlappyPR.HEIGHT-80);
+        lblTitle.setAlignment(Align.center);
+        lblTitle.setFontScale(3);
+
+        lblDeveloper = new Label("Brent Aureli\nMod: Pedro Relvas, 2016", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        lblDeveloper.setSize(50,20);
+        lblDeveloper.setPosition(20, 35);
+        lblDeveloper.setAlignment(Align.left);
+        lblDeveloper.setFontScale(1.5f);
+
+        lblVersion = new Label("V 0.1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        lblVersion.setSize(50,20);
+        lblVersion.setPosition(FlappyPR.WIDTH-70, 20);
+        lblVersion.setAlignment(Align.right);
+        lblVersion.setFontScale(1.5f);
+
+        stage.addActor(lblTitle);
+        stage.addActor(lblDeveloper);
+        stage.addActor(lblVersion);
     }
 }
--- core/src/com/pelezinorr/game/states/PlayState.java
+++ core/src/com/pelezinorr/game/states/PlayState.java
@@ -1,10 +1,17 @@
 package com.pelezinorr.game.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.pelezinorr.game.FlappyPR;
 import com.pelezinorr.game.sprites.Bird;
 import com.pelezinorr.game.sprites.Tube;
@@ -21,18 +28,30 @@ public class PlayState extends State {
     private Texture bg;
     private Texture ground;
     private Vector2 groundPos1, groundPos2;
+    private Label lblScore;
+    private int score;
+    private Stage stage;
+    private Sound soundThrough, soundFail;
+    private boolean gameOver;
+    private Texture playBtn;
 
     private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
+        stage = new Stage();
         bird = new Bird(50, 300);
         bg = new Texture("bg.png");
         ground = new Texture("ground.png");
+        playBtn = new Texture("playbtn.png");
         groundPos1 = new Vector2(cam.position.x - cam.viewportWidth/2, GROUND_Y_OFFSET);
         groundPos2 = new Vector2(cam.position.x - cam.viewportWidth/2 + ground.getWidth(), GROUND_Y_OFFSET);
         tubes = new Array<Tube>();
         cam.setToOrtho(false, FlappyPR.WIDTH/2, FlappyPR.HEIGHT/2);
+        score = 0;
+        initLabels();
+        initSounds();
+        gameOver = false;
 
         for(int i=1; i<=TUBE_COUNT; i++) {
             tubes.add(new Tube(i*(TUBE_SPACING+Tube.TUBE_WIDTH)));
@@ -43,30 +62,46 @@ public class PlayState extends State {
     protected void handleInput() {
         if(Gdx.input.justTouched()) {
             bird.jump();
+            if(gameOver) {
+                gsm.set(new PlayState(gsm));
+                gameOver = false;
+            }
         }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
-        updateGround();
-        bird.update(dt);
-        cam.position.x = bird.getPosition().x + 80;
+        if(!gameOver) {
+            updateGround();
+            bird.update(dt);
+            cam.position.x = bird.getPosition().x + 80;
 
-        for(int i=0; i<tubes.size; i++) {
-            Tube tube = tubes.get(i);
+            for (int i = 0; i < tubes.size; i++) {
+                Tube tube = tubes.get(i);
 
-            if(cam.position.x-(cam.viewportWidth/2) > tube.getPosTopTube().x+tube.getTopTube().getWidth()) { // se o tubo estiver do lado esquerdo
-                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+                if (cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) { // se o tubo estiver do lado esquerdo
+                    tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+                }
+                //check collisions
+                if (tube.collides(bird.getBounds())) {
+                    soundFail.play();
+                    gameOver = true;
+                }
+
+                // check if the bird went through the tubes
+                if ((tube.getPosTopTube().x + tube.TUBE_WIDTH >= bird.getPosition().x - 0.8) && (tube.getPosTopTube().x + tube.TUBE_WIDTH <= bird.getPosition().x + 0.8)) {
+                    soundThrough.play();
+                    incrementScore();
+                }
             }
-            //check collisions
-            if(tube.collides(bird.getBounds())) {
-                gsm.set(new PlayState(gsm));
+
+            if (bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET) {
+                soundFail.play();
+                gameOver = true;
             }
         }
-
-        if(bird.getPosition().y <= ground.getHeight()+GROUND_Y_OFFSET)
-            gsm.set(new PlayState(gsm));
+        stage.act(dt);
         cam.update();
     }
 
@@ -82,7 +117,11 @@ public class PlayState extends State {
         }
         sb.draw(ground, groundPos1.x, groundPos1.y);
         sb.draw(ground, groundPos2.x, groundPos2.y);
+        if(gameOver) {
+            sb.draw(playBtn, cam.position.x - playBtn.getWidth()/2, cam.position.y);
+        }
         sb.end();
+        stage.draw();
     }
 
     @Override
@@ -93,6 +132,8 @@ public class PlayState extends State {
             tube.dispose();
         }
         ground.dispose();
+        soundFail.dispose();
+        soundThrough.dispose();
     }
 
     private void updateGround() {
@@ -102,5 +143,24 @@ public class PlayState extends State {
         if(cam.position.x - cam.viewportWidth/2 > groundPos2.x + ground.getWidth()) {
             groundPos2.add(ground.getWidth()*2, 0);
         }
+    }
+
+    private void initLabels() {
+        lblScore = new Label(score+"", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        lblScore.setSize(50,20);
+        lblScore.setPosition(10, FlappyPR.HEIGHT-50);
+        lblScore.setAlignment(Align.center);
+        lblScore.setFontScale(3);
+
+        stage.addActor(lblScore);
+    }
+
+    private void initSounds() {
+        soundThrough = Gdx.audio.newSound(Gdx.files.internal("acertar.ogg"));
+        soundFail    = Gdx.audio.newSound(Gdx.files.internal("falhar.ogg"));
+    }
+
+    private void incrementScore() {
+        lblScore.setText(++score+"");
     }
 }
